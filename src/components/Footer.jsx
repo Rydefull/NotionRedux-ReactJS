6a7af992c172a4a93fd372b9024def042917a57f
Rydefull <cryir115@gmail.com>
2025-03-13 03:43:27 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="fixed bottom-0 left-0 right-0 flex justify-between items-center bg-gray-200 p-4">
-      <p className="text-left">Created by: Diana Korman</p>
+      <p className="text-left">Created by: Ilya Raschetin</p>
       <p className="text-right">BSU:2024</p>
     </footer>
   );
